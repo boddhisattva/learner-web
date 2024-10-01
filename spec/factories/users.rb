c3861b74f6e6596126e0f0b4659a58ef1a624c11
Mohnish G J <mail4mohnish@gmail.com>
2024-10-01 16:28:03 +0200

    Remove password digest column as its not needed
    Rails console doesn't work with password_digest . Get the following error(using the code here: https://stackoverflow.com/a/60925164/272398):
    
    /Users/sadhak/.rvm/gems/ruby-3.3.5@learner/gems/devise-4.9.4/lib/devise/models/database_authenticatable.rb:197:in `password_digest': wrong number of arguments (given 0, expected 1) (ArgumentError)
    
    password digest was originally required with the Rails implementation of has_secure_password, we are now using Devise for auth.
    
    More details here: https://stackoverflow.com/questions/44946955/devise-token-auth-wrong-number-of-arguments-given-0-expected-1-during-regist

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,17 +2,16 @@
 #
 # Table name: users
 #
-#  id                             :bigint           not null, primary key
-#  email(User email)              :string
-#  encrypted_password             :string           default(""), not null
-#  first_name(User first name)    :string           not null
-#  last_name(User last name)      :string           not null
-#  password_digest(User password) :string
-#  remember_created_at            :datetime
-#  reset_password_sent_at         :datetime
-#  reset_password_token           :string
-#  created_at                     :datetime         not null
-#  updated_at                     :datetime         not null
+#  id                          :bigint           not null, primary key
+#  email(User email)           :string
+#  encrypted_password          :string           default(""), not null
+#  first_name(User first name) :string           not null
+#  last_name(User last name)   :string           not null
+#  remember_created_at         :datetime
+#  reset_password_sent_at      :datetime
+#  reset_password_token        :string
+#  created_at                  :datetime         not null
+#  updated_at                  :datetime         not null
 #
 # Indexes
 #

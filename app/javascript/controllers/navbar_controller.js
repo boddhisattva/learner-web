bca534177173f7aef8fed04d69c7d308156716bc
Mohnish G J <mail4mohnish@gmail.com>
2024-10-02 06:33:39 +0200

    Add more clarity to what the gems stand for & remove invalid JS

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,13 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "burger", "menu" ]
 
-  console.log("I am in the navbar controller");
-
-  debugger;
-
   toggle() {
-    console.log("I am in the toggle method of navbar controller")
-
     this.burgerTarget.classList.toggle("is-active")
     this.menuTarget.classList.toggle("is-active")
   }

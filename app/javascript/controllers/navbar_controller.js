d5c8132b4815a08f832c6b519cbb0e195daf7050
Mohnish G J <mail4mohnish@gmail.com>
2024-10-03 10:56:38 +0200

    Check ability to debug JS side of things in Stimulus & Hotwire

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,6 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "burger", "menu" ]
 
+  connect() {
+    console.log('Hello from Stimulus', this.element);
+  }
+
+
   toggle() {
     this.burgerTarget.classList.toggle("is-active")
     this.menuTarget.classList.toggle("is-active")

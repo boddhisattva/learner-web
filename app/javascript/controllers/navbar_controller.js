d40c410d46561c23f3031426b1bf80761c792763
Mohnish G J <mail4mohnish@gmail.com>
2024-10-06 05:13:26 +0200

    Remove debug statement as was only added for test purpose
    - Originally added to only test how debugging works with Stimulus controller

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,11 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "burger", "menu" ]
 
-  connect() {
-    console.log('Hello from Stimulus', this.element);
-  }
-
-
   toggle() {
     this.burgerTarget.classList.toggle("is-active")
     this.menuTarget.classList.toggle("is-active")

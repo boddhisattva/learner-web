30eb2635e48a41d8d5e4eede51d2692db165345e
Mohnish G J <mail4mohnish@gmail.com>
2024-10-06 14:20:32 +0200

    Fix lint failure

--- config/deploy/puma.rb
+++ config/deploy/puma.rb
@@ -26,7 +26,8 @@ threads threads_count, threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
-# Specifies the number of `workers` to boot in clustered mode. # Workers are forked web server processes. If using threads and workers together
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).

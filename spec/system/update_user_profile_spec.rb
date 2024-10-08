f4d64c6d95bbfe4b7cad8b5f69f2efe54033444e
Mohnish G J <mail4mohnish@gmail.com>
2024-10-08 17:01:25 +0200

    Allow updating users & seamlessly displaying name on update
    - This uses Turbo Frames and Turbo Streams functionality as part of Hotwire Turbo under the hood

---
+++ spec/system/update_user_profile_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'Update user profile details', type: :system do
+  describe 'User sign up flow' do
+    let(:user) { create(:user, first_name: '  Rachel ', last_name: ' Longwood', email: '  rachel@xyz.com ') }
+
+    before { sign_in user }
+
+    it 'Updates user details like first name, email etc., with valid inputs & return success message' do
+      visit '/profile'
+
+      fill_in User.human_attribute_name(:first_name), with: 'Rania'
+
+      click_button I18n.t("users.show.save_profile")
+
+      expect(page).to have_selector("#current_user_name", text: "Rania Longwood")
+      expect(page).to have_text(I18n.t("users.update.success"))
+    end
+  end
+end

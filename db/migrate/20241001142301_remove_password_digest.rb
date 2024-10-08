f7582b5a402d3ae761cfb94e88cb521061791564
Mohnish G J <mail4mohnish@gmail.com>
2024-10-08 21:58:08 +0200

    Allow ease of making remove password_digest migration reversible

--- db/migrate/20241001142301_remove_password_digest.rb
+++ db/migrate/20241001142301_remove_password_digest.rb
@@ -1,5 +1,5 @@
 class RemovePasswordDigest < ActiveRecord::Migration[7.2]
   def change
-    safety_assured { remove_column :users, :password_digest }
+    safety_assured { remove_column :users, :password_digest, :string }
   end
 end

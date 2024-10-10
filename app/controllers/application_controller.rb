f0dd116b543585c61f545a26ac9f4998d16d1b8c
Mohnish G J <mail4mohnish@gmail.com>
2024-10-10 14:35:50 +0200

    Hide navigation bar in native apps as all navigation components will be native
    More context:
    
    There's a navigation bar across the top of the screen on web. In the apps, all navigation components will be native which makes that bar redundant. It needs to be hidden for requests made from the apps.
    
    Related changes include:
    - Add a link to the Sign Up page on the Log In page
    - Add a link to the Log In page on the Sign Up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  helper_method :turbo_native_app?
+
   def after_sign_in_path_for(resource)
     feed_index_path
   end

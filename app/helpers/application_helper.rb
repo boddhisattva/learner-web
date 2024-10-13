84e5e62581b246a23c0142e563e7c560619a3416
Mohnish G J <mail4mohnish@gmail.com>
2024-10-13 05:44:53 +0200

    Display only content title without app name for native mobile app
    - App title not needed for native mobile app
    -  Helper tests don’t run within the context of a controller(specifically here we are referring to app controller)
    - hence doesn’t have access to the turbo_native_app? method.
    - Since Helper modules are included in the test case automatically, we only need to define a custom turbo_native_app?  method to fix the failing spec which happenned to fail as a result of the code change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,9 @@ module ApplicationHelper
   def title
     return t("learner") unless content_for?(:title)
 
+    # TODO: Test this manually later once you continue development for the mobile native app
+    return content_for(:title) if turbo_native_app?
+
     "#{content_for(:title)} | #{t("learner")}"
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  before { turbo_native_app? }
+
   describe '#title' do
     it 'page specific title in the context of the app name' do
       content_for(:title) { "Page Title" }
@@ -12,5 +14,15 @@ RSpec.describe ApplicationHelper, type: :helper do
     it 'returns app name when page title is missing' do
       expect(title).to eq("#{I18n.t('learner')}")
     end
+
+    it 'returns page specific title only for turbo native' do
+      @turbo_native_app = true
+
+      expect(title).to eq("#{I18n.t('learner')}")
+    end
+  end
+
+  def turbo_native_app?
+    @turbo_native_app = false
   end
 end

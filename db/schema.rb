529abab168ef2ba5972bc54564dfd4dab0410357
Mohnish G J <mail4mohnish@gmail.com>
2024-10-21 05:21:52 +0200

    Add organization name column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_20_080014) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_20_154306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_20_080014) do
   create_table "organizations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.index ["name"], name: "index_organizations_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|

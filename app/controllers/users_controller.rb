ef444c50586fe8407bd8721f45971085a619ba31
Mohnish G J <mail4mohnish@gmail.com>
2024-10-21 07:11:30 +0200

    Ensure user signup creates an organisation with user name & other misc changes
    Misc changes
    - Increase max line length to 135
    - Fix failing specs
    - Add Test for missing expectations like membership count gets updated on user sign up, etc.,
    - use separate specs for sign up happy path & errors as its easier to have & manage different specs for different scenarios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+class NameUpdateError < StandardError; end
+
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [ :update ]
 
@@ -11,9 +13,11 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
 
-      @organization = Organization.create(members: [ @user ])
+      # Every user gets added to their own 'self' related organization on creation
+      # TO DO: Come back to expore how to handle the below code if oragnization creation fails & add relevant spec
+      @organization = Organization.create(members: [ @user ], name: @user.name)
 
-      redirect_to feed_index_path,
+      redirect_to learnings_index_path,
         status: :see_other,
         flash: { success: t(".welcome", name: @user.name) }
     else
@@ -23,16 +27,33 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
+    user_organization = current_user.own_organization
+
+    ActiveRecord::Base.transaction do
+      raise NameUpdateError unless current_user.update(user_params)
+
+      user_organization.name = current_user.name
+      if user_organization.changed?
+        # TO DO: Come back to expore how to handle the below code if oragnization updation fails & add relevant spec
+        raise NameUpdateError unless user_organization&.update(name: current_user.name)
+      end
+
       flash[:success] = t(".success")
       redirect_to profile_path, status: :see_other
-    else
-      flash.now[:error] = current_user.errors.full_messages
-      render :edit, status: :unprocessable_entity
     end
+  rescue NameUpdateError
+      # TO DO: Come back to expore how to handle the below code if oragnization creation fails..
+      # TO DO Continuation: ..we need to return relevant errors of failed oragnization creation in a relevant spec & add relevant spec
+      flash.now[:error] = current_user.errors.full_messages.concat(user_organization.errors.full_messages)
+      render :edit, status: :unprocessable_entity
   end
 
   private
+    def user_organization_updated_on_name_update?
+      user_organization.name = current_user.name
+      user_organization&.update(name: current_user.name) if user_organization.changed?
+    end
+
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password)
     end

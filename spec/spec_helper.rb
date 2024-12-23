4289fdd0c33faa1bd87fbb6ff15f1c86037006d6
Mohnish G J <mail4mohnish@gmail.com>
2024-12-23 16:15:23 +0100

    Ensure ability to run system specs independently on Rails 8
    - They were failing earlier without this addition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -91,7 +91,13 @@ RSpec.configure do |config|
   #   # test failures related to randomization by passing the same `--seed` value
   #   # as the one that triggered the failure.
   #   Kernel.srand config.seed
+  # TODO: Remove this once https://github.com/heartcombo/devise/issues/5705 related fix is released via a new Devise version
   config.before(:each, type: :controller) do
+    Rails.application.reload_routes_unless_loaded
+  end
+
+  # TODO: Remove this once https://github.com/heartcombo/devise/issues/5705 related fix is released via a new Devise version
+  config.before(:each, type: :system) do
     Rails.application.reload_routes_unless_loaded
   end
 end

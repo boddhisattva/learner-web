96068ef2726e8d0a2e282407429fa8fe4e757985
Mohnish G J <mail4mohnish@gmail.com>
2025-12-30 17:22:21 +0100

    Ensure on visiting home page user is taken to /learnings path
    - Earlier it was taking user to http://localhost:3000/learnings/index which is incorrect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   def after_sign_in_path_for(_resource)
-    learnings_index_path
+    learnings_path
   end
 end

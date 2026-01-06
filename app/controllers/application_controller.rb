ce9de4e14ef9fc5d41fe76c0b78f0fbe9d4f3b56
Mohnish G J <mail4mohnish@gmail.com>
2026-01-06 19:58:03 +0100

    Store current oganisation in session to scope learnings to org
    - this can be helpful for things like counter cache implementation etc.,

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :set_current_organization
+
   def after_sign_in_path_for(_resource)
     learnings_path
   end
@@ -21,4 +23,37 @@ class ApplicationController < ActionController::Base
       Prosopite.finish
     end
   end
+
+  private
+
+    def current_organization
+      @current_organization ||= find_current_organization
+    end
+
+    def current_membership
+      @current_membership ||= find_current_membership
+    end
+
+    def find_current_organization
+      return nil unless current_user
+
+      if session[:current_organization_id]
+        org = current_user.organizations.find_by(id: session[:current_organization_id])
+        return org if org.present?
+      end
+
+      org = current_user.personal_organization
+      org.presence
+    end
+
+    def find_current_membership
+      return nil unless current_organization
+
+      current_user.memberships.find_by(organization_id: current_organization.id)
+    end
+
+    def set_current_organization
+      # Store in session for next request
+      session[:current_organization_id] = current_organization&.id
+    end
 end
